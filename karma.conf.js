226c8fa42f4c775bfbcda99268ddbed2d4714bd4
avillegas <correodebitter@gmail.com>
2022-04-06 18:54:28 -0300

    1stPush

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/New'),
+      dir: require('path').join(__dirname, './coverage/third-project'),
       subdir: '.',
       reporters: [
         { type: 'html' },
